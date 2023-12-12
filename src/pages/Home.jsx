00d79b95f43dbffd99570399327347047010e3c4
Artem Borodovoy <a.borodovoi@gmail.com>
2023-12-12 19:53:50 +0200

    added search and paginnnation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import { Skeleton } from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -21,9 +23,10 @@ const Home = () => {
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'ask' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     fetch(
-      `https://6574c5a3b2fbb8f6509ca5cb.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://6574c5a3b2fbb8f6509ca5cb.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((response) => {
         return response.json();
@@ -33,7 +36,26 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  // ==========================================================
+  // Search for static
+  // const pizzas = items
+  // .filter((obj) => {
+  //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+  //     return true;
+  //   }
+
+  //   return false;
+  // })
+  // .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  // ==========================================================
+
+  const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
+
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
     <div className="container">
@@ -49,10 +71,9 @@ const Home = () => {
         {/* {items.map((pizza) => {
               return <PizzaBlock key={pizza.id} {...pizza} />;
             })} */}
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+        {isLoading ? skeletons : pizzas}
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
