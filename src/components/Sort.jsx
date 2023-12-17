3d691c6114981d8abe81398963f1968cc6508152
Artem Borodovoy <a.borodovoi@gmail.com>
2023-12-17 13:53:42 +0200

    added popup  to reedux

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSort } from '../redux/slices/filterSlices';
 
@@ -14,6 +14,7 @@ export const sortlist = [
 function Sort() {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = useRef();
 
   const [open, setOpen] = useState(false);
 
@@ -22,8 +23,22 @@ function Sort() {
     setOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutSide = (event) => {
+      const path = event.composedPath ? event.composedPath() : event.path;
+
+      if (!path.includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutSide);
+
+    return () => document.body.removeEventListener('click', handleClickOutSide);
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
